68ecf6aeb69cb573025c09ae47fb67113af77f7a
ocularrhythm <aaron.g.arney@gmail.com>
2018-06-25 19:17:11 -0400

    Add rotational behavior

--- Assets/UdacityVR/Scripts/Coin.cs
+++ Assets/UdacityVR/Scripts/Coin.cs
@@ -4,30 +4,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour {
 
-	// TODO: Create variables to reference the game objects we need access to
-	// Declare a GameObject named 'coinPoofPrefab' and assign the 'CoinPoof' prefab to the field in Unity
 	public GameObject coinPoofPrefab;
+	public int rotationSpeed = 25;
+	public float destroyAfterSeconds = 0.5f;
 
 	void Update () {
-		// OPTIONAL-CHALLENGE: Animate the coin rotating
-		// TIP: You could use a method from the Transform class
+		this.transform.Rotate(new Vector3(0, this.rotationSpeed, 0) * Time.deltaTime);
 	}
 
 
 	public void OnCoinClicked () {
-		/// Called when the 'Coin' game object is clicked
-		/// - Displays a poof effect (handled by the 'CoinPoof' prefab)
-		/// - Plays an audio clip (handled by the 'CoinPoof' prefab)
-		/// - Removes the coin from the scene
-
-		// Prints to the console when the method is called
 		Debug.Log ("'Coin.OnCoinClicked()' was called");
 
-		// TODO: Display the poof effect and remove the coin from the scene
-		// Use Instantiate() to create a clone of the 'CoinPoof' prefab at this coin's position and with the 'CoinPoof' prefab's rotation
-		// Use Destroy() to delete the coin after for example 0.5 seconds
 		Instantiate(this.coinPoofPrefab, this.transform.position, this.coinPoofPrefab.transform.rotation);
 
-		Destroy(this.gameObject, 0.5f);
+		Destroy(this.gameObject, this.destroyAfterSeconds);
 	}
 }
