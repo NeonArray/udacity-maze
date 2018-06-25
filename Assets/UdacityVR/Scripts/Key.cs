668642353b6d4e812ec022e50ae4803fa3efc4b0
ocularrhythm <aaron.g.arney@gmail.com>
2018-06-25 19:33:02 -0400

    Remove comments

--- Assets/UdacityVR/Scripts/Key.cs
+++ Assets/UdacityVR/Scripts/Key.cs
@@ -15,17 +15,12 @@ public class Key : MonoBehaviour {
 
 
 	public void OnKeyClicked () {
-
 		Debug.Log ("'Key.OnKeyClicked()' was called");
 
-		// TODO: Unlock the door, display the poof effect, and remove the key from the scene
-		// Use 'door' to call the Door.Unlock() method
 		this.door.GetComponentInChildren<Door>().Unlock();
 
-		// Use Instantiate() to create a clone of the 'KeyPoof' prefab at this coin's position and with the 'KeyPoof' prefab's rotation
 		Instantiate(this.keyPoofPrefab, this.transform.position, this.keyPoofPrefab.transform.rotation);
 
-		// Use Destroy() to delete the key after for example 0.5 seconds
 		Destroy(this.gameObject, this.destroyAfterSeconds);
 	}
 }
