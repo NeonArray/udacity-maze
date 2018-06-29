9c1e4587556e8e80409220c10eec6c389f70518c
ocularrhythm <aaron.g.arney@gmail.com>
2018-06-28 21:14:03 -0400

    Build out maze

---
+++ Assets/UdacityVR/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public GameObject[] spawnPoints;
+	public GameObject player;
+
+	void Start ()
+	{
+		int index = Random.Range(0, this.spawnPoints.Length);
+
+		this.player.transform.position = this.spawnPoints[index].transform.position;
+	}
+}
